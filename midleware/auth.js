ee24ff9fa9638845c76c58f16656a64d519b3ac7
anditiana <andi.id.dev@gmail.com>
2023-07-23 23:09:21 +0700

    third attemp

--- midleware/auth.js
+++ midleware/auth.js
@@ -4,7 +4,7 @@ module.exports = {
   verifyToken : (req, res, next) => {
     try {
       const token = req.headers.authorization.split(' ')[1];
-      const verifiedToken = jwt.verify(token, 'testing123');
+      const verifiedToken = jwt.verify(token, process.env.KEY_TOKEN_CREDENTIAL);
       req.user = verifiedToken;
       next();
     } catch (error) {
