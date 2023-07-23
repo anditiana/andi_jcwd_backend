37c68d9d54093f3bd5b16f0e234e291ff9a6ec80
anditiana <andi.id.dev@gmail.com>
2023-07-23 23:11:15 +0700

    index.js update

--- index.js
+++ index.js
@@ -6,32 +6,20 @@ const router = require('./router/router');
 require('dotenv').config();
 
 
+try {
+  if (process.env.USERNAME === process.env.USER_ORIGINAL) {
+    server.use(express.json());
+    server.use(express.urlencoded({extended : true}));
 
-console.log(process.env.USERNAME);
-server.use(express.json());
-server.use(express.urlencoded({extended : true}));
+    server.use(router);
 
-server.use(router);
-
-server.listen(process.env.PORT_ENV, ()=>{
-  // db.sequelize.sync({ alter: true });
-  console.log(`Server success running on port : ${process.env.PORT_ENV} | ${process.env.ORIGINAL_CREATOR}`);
-});
-
-// try {
-//   if (process.env.USERNAME === process.env.USER_ORIGINAL) {
-//     server.use(express.json());
-//     server.use(express.urlencoded({extended : true}));
-
-//     server.use(router);
-
-//     server.listen(process.env.PORT_ENV, ()=>{
-//       // db.sequelize.sync({ alter: true });
-//       console.log(`Server success running on port : ${process.env.PORT_ENV} | ${process.env.ORIGINAL_CREATOR}`);
-//     });
-//   }else{
-//     throw new Error({message : "Unidentified User"})
-//   }
-// } catch (error) {
-//   console.log(error);
-// }
+    server.listen(process.env.PORT_ENV, ()=>{
+      // db.sequelize.sync({ alter: true });
+      console.log(`Server success running on port : ${process.env.PORT_ENV} | ${process.env.ORIGINAL_CREATOR}`);
+    });
+  }else{
+    throw new Error({message : "Unidentified User"})
+  }
+} catch (error) {
+  console.log(error);
+}
