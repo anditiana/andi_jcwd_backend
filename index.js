7e0a5605bf26bfdb941c9e71396e2730ce04f9cb
anditiana <andi.id.dev@gmail.com>
2023-07-24 14:46:04 +0700

    original credential

--- index.js
+++ index.js
@@ -16,21 +16,3 @@ server.listen(process.env.PORT_ENV, ()=>{
   // db.sequelize.sync({ alter: true });
   console.log(`Server success running on port : ${process.env.PORT_ENV} | ${process.env.ORIGINAL_CREATOR}`);
 });
-
-// try {
-//   if (process.env.USERNAME === process.env.USER_ORIGINAL) {
-//     server.use(express.json());
-//     server.use(express.urlencoded({extended : true}));
-
-//     server.use(router);
-
-//     server.listen(process.env.PORT_ENV, ()=>{
-//       // db.sequelize.sync({ alter: true });
-//       console.log(`Server success running on port : ${process.env.PORT_ENV} | ${process.env.ORIGINAL_CREATOR}`);
-//     });
-//   }else{
-//     throw new Error({message : "Unidentified User"})
-//   }
-// } catch (error) {
-//   console.log(error);
-// }
